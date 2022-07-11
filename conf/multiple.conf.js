3c30ee58336aeb0d22af2f9f8425ee00c1af45a5
likith-lt <likithk@lambdatest.com>
2022-07-11 17:34:47 +0530

    code fixes

--- conf/multiple.conf.js
+++ conf/multiple.conf.js
@@ -1,37 +1,39 @@
 exports.config = {
-   user: process.env.LT_USERNAME || 'YOUR LAMBDATEST USERNAME',
-  key: process.env.LT_ACCESS_KEY || 'YOUR LAMBDATEST ACCESS KEY',
+  user: process.env.LT_USERNAME || "YOUR LAMBDATEST USERNAME",
+  key: process.env.LT_ACCESS_KEY || "YOUR LAMBDATEST ACCESS KEY",
 
   updateJob: false,
-  specs: [
-    './tests/specs/multiple_test.js'
-  ],
+  specs: ["./tests/specs/multiple_test.js"],
   exclude: [],
 
- capabilities: [{
-    browserName: 'chrome',
-    version: '65.0',
-    platform: 'WIN10',
-    name: 'webdriverIO-Multiple_test',
-    build: 'webdriverIO-lambdatest',
-    visual:false,
-    video:true,
-    console:false,
-    network:false
-  }],
+  capabilities: [
+    {
+      browserName: "chrome",
+      version: "65.0",
+      platform: "WIN10",
+      name: "webdriverIO-Multiple_test",
+      build: "webdriverIO-lambdatest",
+      visual: false,
+      video: true,
+      console: false,
+      network: false,
+    },
+  ],
 
-  logLevel: 'verbose',
+  logLevel: "info",
   coloredLogs: true,
-  screenshotPath: './errorShots/',
-  baseUrl: '',
+  screenshotPath: "./errorShots/",
+  baseUrl: "",
   waitforTimeout: 10000,
   connectionRetryTimeout: 90000,
   connectionRetryCount: 3,
-  host: 'hub.lambdatest.com',
-  port:'80',
-  
-  framework: 'mocha',
+  path: "/wd/hub",
+  hostname: "hub.lambdatest.com",
+  port: 80,
+
+  framework: "mocha",
   mochaOpts: {
-      ui: 'bdd'
-  }
-}
+    ui: "bdd",
+    timeout: 20000,
+  },
+};
--- conf/parallel.conf.js
+++ conf/parallel.conf.js
@@ -1,59 +1,64 @@
 exports.config = {
-  user: process.env.LT_USERNAME || 'YOUR LAMBDATEST USERNAME',
-  key: process.env.LT_ACCESS_KEY || 'YOUR LAMBDATEST ACCESS KEY',
+  user: process.env.LT_USERNAME || "YOUR LAMBDATEST USERNAME",
+  key: process.env.LT_ACCESS_KEY || "YOUR LAMBDATEST ACCESS KEY",
 
   updateJob: false,
-  specs: [
-    './tests/specs/single_test.js'
-  ],
+  specs: ["./tests/specs/single_test.js"],
   exclude: [],
 
   maxInstances: 10,
   commonCapabilities: {
-    name: 'webdriverIO-parallel_test',
-    build: 'webdriverIO-lambdatest',
-    visual:false,
-    video:true,
-    console:false,
-    network:false
+    name: "webdriverIO-parallel_test",
+    build: "webdriverIO-lambdatest",
+    visual: false,
+    video: true,
+    console: false,
+    network: false,
   },
 
-  capabilities: [{
-    browserName: 'chrome',
-    version: '65.0',
-    platform: 'WIN10'
-  },{
-    browser: 'firefox',
-    version: '64.0',
-    platform: 'WIN7',
-  },{
-    browser: 'internet explorer'
-    version: '10.0',
-    platform: 'WIN10',
-  },{
-    browser: 'MicrosoftEdge'
-    version: '18.0',
-    platform: 'WIN10',
-  }],
+  capabilities: [
+    {
+      browserName: "chrome",
+      version: "65.0",
+      platform: "WIN10",
+    },
+    {
+      browser: "firefox",
+      version: "64.0",
+      platform: "WIN7",
+    },
+    {
+      browser: "internet explorer",
+      version: "10.0",
+      platform: "WIN10",
+    },
+    {
+      browser: "MicrosoftEdge",
+      version: "18.0",
+      platform: "WIN10",
+    },
+  ],
 
-  logLevel: 'verbose',
+  logLevel: "info",
   coloredLogs: true,
-  screenshotPath: './errorShots/',
-  baseUrl: '',
+  screenshotPath: "./errorShots/",
+  baseUrl: "",
   waitforTimeout: 10000,
   connectionRetryTimeout: 90000,
   connectionRetryCount: 3,
-  host: 'hub.lambdatest.com',
-  port:'80',
-  
-  framework: 'mocha',
+  path: "/wd/hub",
+  hostname: "hub.lambdatest.com",
+  port: 80,
+
+  framework: "mocha",
   mochaOpts: {
-      ui: 'bdd'
-  }
-}
+    ui: "bdd",
+    timeout: 20000,
+  },
+};
 
 // Code to support common capabilities
-exports.config.capabilities.forEach(function(caps){
-  for(var i in exports.config.commonCapabilities) caps[i] = caps[i] || exports.config.commonCapabilities[i];
+exports.config.capabilities.forEach(function (caps) {
+  for (var i in exports.config.commonCapabilities)
+    caps[i] = caps[i] || exports.config.commonCapabilities[i];
 });
-
