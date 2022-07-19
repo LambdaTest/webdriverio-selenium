be96b4c52d928db3918d663283b11394bc3cc8e4
Mukesh Rana <mukeshr@Admins-MacBook-Pro.local>
2022-07-19 18:17:02 +0530

    fixup

--- tests/specs/multiple/test_01.js
+++ tests/specs/multiple/test_01.js
@@ -5,7 +5,7 @@ describe('Lambdatest Demo Test', function() {
     await browser.url('https://lambdatest.github.io/sample-todo-app/')
     const firstElement = await browser.$('*[name="li1"]');
     await firstElement.click();
-    const input = await browser.$('*[id="sampletodotext"]','Lambdatest\n');
+    const input = await browser.$('*[id="sampletodotext"]');
     await input.setValue('Lambdatest\n');
     const title = await browser.getTitle();
     assert(title.match(/Sample page - lambdatest.com/i));
--- tests/specs/multiple/test_03.js
+++ tests/specs/multiple/test_03.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 describe('Lambdatest Demo Test', function() {
   it('Lambdatest Demo Testcase', async function () {
     await browser.url('https://lambdatest.github.io/sample-todo-app/')
-    const input = await browser.$('*[id="sampletodotext"]','Lambdatest\n');
+    const input = await browser.$('*[id="sampletodotext"]');
     await input.setValue('Lambdatest\n');
     const title = await browser.getTitle();
     assert(title.match(/Sample page - lambdatest.com/i));
